237b22f4d0c896f3e019092145f43aa27299dfa9
Narensvc <88049804+Narensvc@users.noreply.github.com>
2022-09-29 14:22:48 +0530

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "jenkins"{
+    ami = "ami-026b57f3c383c2eec"
+    instance_type = "t2.micro"
+    count = 1
+    key_name = "terraform"
+
+    tags = {
+        "Name" = "jenkins"
+     }
+}
